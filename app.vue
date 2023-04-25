f108391b6daacf84387b6759323dc67d2a9a735f
thomaseilander <thomaseilander80@gmail.com>
2023-04-25 09:33:06 +0200

    next update

--- app.vue
+++ app.vue
@@ -3,3 +3,12 @@
     Hello world this is development v5!
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Learning NUXT',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ]
+})
+</script>
