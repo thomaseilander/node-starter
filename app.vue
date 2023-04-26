181c45842bc88815747b50419dfb96ecd4edee29
thomaseilander <thomaseilander80@gmail.com>
2023-04-27 00:00:34 +0200

    tailwind installed

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
+  <h1 class="text-3xl font-bold underline">
     Hello world this is development v7!
+  </h1>
   </div>
 </template>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+import { resolve } from "path";
 
-})
+export default defineNuxtConfig({
+    alias: {
+        "@": resolve(__dirname, "/")
+    },
+    css: ["~/assets/main.css"],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+});
